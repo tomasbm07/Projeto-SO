74c15ed7ebac63dd2d6a91457acb32f92f11e923
Tomás Mendes <43079732+tomasbm07@users.noreply.github.com>
2021-03-24 12:01:57 +0000

    Minor changes

--- Projeto/src/Race_Simulator.c
+++ Projeto/src/Race_Simulator.c
@@ -18,26 +18,28 @@ int main(int argc, char* argv[]) {
     exit(1);
   }
 
-  // read confif file
+  // read config file
   read_file(argv[1]);
+
 #ifdef DEBUG
   write_log("Successfully read config file");
 #endif
 
-  /*
+#ifdef DEBUG
   printf("Configurações lidas do ficheiro:\n");
   printf("Numero de unidade de tempo /s: %dut\n", nr_unid_tps);
-  printf("Distancia de uma volta: %dm, Numero de voltas da corrida:
-  %d\n",dist_volta, nr_voltas); printf("Numero de equipas: %d\n", nr_equipas);
-  printf("Numero de unidades de tempo entre possivel avaria: %dut\n",
-  unid_temp_avaria); printf("Tempo min de reparacao: %dut, Tempo max de
-  reparacao: %dut\n", min_reparacao, max_reparacao); printf("Capacidade
-  dodeposito: %dL\n", deposito); printf("--------------------------\n");
+  printf("Distancia de uma volta: %dm, Numero de voltas da corrida: %d\n",dist_volta, nr_voltas); 
+  printf("Numero de equipas: %d\n", nr_equipas);
+  printf("Numero de unidades de tempo entre possivel avaria: %dut\n", unid_temp_avaria); 
+  printf("Tempo min de reparacao: %dut, Tempo max dereparacao: %dut\n", min_reparacao, max_reparacao); 
+  printf("Capacidade do deposito: %dL\n", deposito); 
+  printf("--------------------------\n");
   printf("ut - unidades de tempo\n");
   printf("m - metros\n");
   printf("L - litros\n");
   printf("--------------------------\n\n\n");
-  */
+#endif
+
 
   // shared mem
   init_shm();
@@ -51,8 +53,6 @@ int main(int argc, char* argv[]) {
 
   // wait for both process to finish
   for (i = 0; i < 2; i++) wait(NULL);
-
-  // write_log("Teste 123");
 
   // destroy shared mem and semaphores
   terminate_shm();
--- Projeto/src/write_output.c
+++ Projeto/src/write_output.c
@@ -6,8 +6,8 @@ void write_log(char *line){
     time_t my_time;
     struct tm * timeinfo; 
 
-    time (&my_time); //gets the curent time
-    timeinfo = localtime (&my_time); //converts time into struct timeinfo
+    time(&my_time); //gets the curent time
+    timeinfo = localtime(&my_time); //converts time into struct timeinfo
 
     f = fopen("log.txt", "a");
     fprintf(stdout, "[%d:%d:%d] %s\n", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, line);
