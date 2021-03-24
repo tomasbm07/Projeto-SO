b0282efacc0ac3704546d76bb6393c24c5a3ae13
Tomás Mendes <43079732+tomasbm07@users.noreply.github.com>
2021-03-24 10:07:27 +0000

    cenas

--- Projeto/src/Race_Simulator.c
+++ Projeto/src/Race_Simulator.c
@@ -1,35 +1,55 @@
+#include "carro.h"
 #include "config_file.h"
+#include "malfunctin_manager.h"
+#include "race_manager.h"
+#include "team_manager.h"
+#include "write_output.h"
+
 // add includes
 
+//global variables
+
+
+
 int main(int argc, char* argv[]) {
+  int i;
+
   if (argc != 2) {
     printf("No config file was passed!\n");
     exit(0);
   }
 
   read_file(argv[1]);
-  
+
   /*printf("Configurações lidas do ficheiro:\n");
   printf("Numero de unidade de tempo /s: %dut\n", nr_unid_tps);
-  printf("Distancia de uma volta: %dm, Numero de voltas da corrida: %d\n", dist_volta, nr_voltas);
-  printf("Numero de equipas: %d\n", nr_equipas);
-  printf("Numero de unidades de tempo entre possivel avaria: %dut\n", unid_temp_avaria);
-  printf("Tempo min de reparacao: %dut, Tempo max de reparacao: %dut\n", min_reparacao, max_reparacao);
-  printf("Capacidade do deposito: %dL\n", deposito);
-  printf("--------------------------\n");
+  printf("Distancia de uma volta: %dm, Numero de voltas da corrida: %d\n",
+  dist_volta, nr_voltas); printf("Numero de equipas: %d\n", nr_equipas);
+  printf("Numero de unidades de tempo entre possivel avaria: %dut\n",
+  unid_temp_avaria); printf("Tempo min de reparacao: %dut, Tempo max de
+  reparacao: %dut\n", min_reparacao, max_reparacao); printf("Capacidade do
+  deposito: %dL\n", deposito); printf("--------------------------\n");
   printf("ut - unidades de tempo\n");
   printf("m - metros\n");
   printf("L - litros\n");
   printf("--------------------------\n");*/
 
-  if (!fork())
-  	race_manager();
-  if (!fork())
-	malfunction_manager();
-  
-  int i;
-  for(i=0; i < 2; i++)
-  	wait(NULL);
-  	
+  //shared mem
+
+  //shared mem var
+
+  //create race manager process
+  if (!fork()) race_manager(); //TODO 
+
+  //create malfunction manager
+  if (!fork()) malfunction_manager();
+
+
+  //wait for all process to finish
+  for (i = 0; i < 2; i++) wait(NULL);
+
+
+  //destroy shared mem and semaphores
+
   exit(0);
 }
